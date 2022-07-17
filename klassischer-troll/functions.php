ece37de6947b8897338e945b19af70c3374a6942
sorid <thoms77@gmx.net>
2022-07-17 12:53:50 +0200

    Removed google fonts

--- klassischer-troll/functions.php
+++ klassischer-troll/functions.php
@@ -3,16 +3,18 @@
 /* functions für childtheme Klassischer Troll */
 
 function twentyfifteen_child_styles() {
-wp_deregister_style( 'twentyfifteen-style');
-wp_register_style('twentyfifteen-style', get_template_directory_uri(). '/style.css');
-wp_enqueue_style('twentyfifteen-style', get_template_directory_uri(). '/style.css');
-wp_enqueue_style( 'childtheme-style', get_stylesheet_directory_uri().'/style.css', array('twentyfifteen-style') );
+  wp_deregister_style( 'twentyfifteen-style');
+  wp_register_style('twentyfifteen-style', get_template_directory_uri(). '/style.css');
+  wp_enqueue_style('twentyfifteen-style', get_template_directory_uri(). '/style.css');
+  wp_enqueue_style( 'childtheme-style', get_stylesheet_directory_uri().'/style.css', array('twentyfifteen-style') );
 }
-add_action( 'wp_enqueue_scripts', 'twentyfifteen_child_styles' );
 
- /* Bei Kommentaren die IP nicht mitloggen
-function  wpb_remove_commentsip( $comment_author_ip ) {
-	return '';
-	}
-add_filter( 'pre_comment_user_ip', 'wpb_remove_commentsip' );
-*/
+/* Goolegfonst entfernen */
+function wpse_dequeue_google_fonts() {
+    wp_dequeue_style( 'twentyfifteen-fonts' );
+}
+
+add_action( 'wp_enqueue_scripts', 'wpse_dequeue_google_fonts', 20 );
+
+/* Child Theme laden */
+add_action( 'wp_enqueue_scripts', 'twentyfifteen_child_styles' );
